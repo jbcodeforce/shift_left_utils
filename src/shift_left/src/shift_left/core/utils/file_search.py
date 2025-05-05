920d326b7da6c6b0c99700f81e850beb2222a491
Robby Canady <rcanady@mastercontrol.com>
2025-05-05 13:10:27 -0600

    Updating to use the naming convention for retrieving compute pools and statements

--- src/shift_left/src/shift_left/core/utils/file_search.py
+++ src/shift_left/src/shift_left/core/utils/file_search.py
@@ -63,8 +63,7 @@ class FlinkTablePipelineDefinition(InfoNode):
         return self.table_name == other.table_name
      
     def to_node(self) -> FlinkStatementNode:
-        dml_statement_name = "dml-" + self.table_name.replace("_", "-")
-        ddl_statement_name = "ddl-" + self.table_name.replace("_", "-")
+        ddl_statement_name, dml_statement_name = get_ddl_dml_names_from_table(self.table_name)
         r = FlinkStatementNode(table_name= self.table_name,
                                path= self.path,
                                created_at=datetime.now(),
@@ -311,13 +310,18 @@ def get_or_build_source_file_inventory(src_path: str) -> Dict[str, str]:
     file_paths.update(list_src_sql_files(f"{src_path}/stage"))
     return file_paths
 
-def get_ddl_dml_names_from_table(table_name: str) -> Tuple[str,str]:
+def get_ddl_dml_names_from_table(table_name: str, ) -> Tuple[str,str]:
     ddl_n = "ddl-" + table_name.replace("_","-")
     dml_n = "dml-" + table_name.replace("_","-")
     return ddl_n, dml_n
 
 def get_ddl_dml_names_from_pipe_def(to_process: FlinkTablePipelineDefinition) -> Tuple[str,str]:
-    return get_ddl_dml_names_from_table(to_process.table_name)
+    node = to_process.to_node()
+    ddl_n, dml_n = get_ddl_dml_names_from_table(to_process.table_name)
+    node.ddl_statement_name = ddl_n
+    node.dml_statement_name = dml_n
+    _apply_naming_convention(node)
+    return (node.ddl_n, node.dml_n)
 
 def get_ddl_file_name(folder_path: str) -> str:
     """
