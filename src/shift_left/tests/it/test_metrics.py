4aaf6bc183f54f0bfe40690396e3676a94706da2
sraj2023 <sraj@confluent.io>
2025-06-24 23:25:22 +0530

    Changes for Integration Tests

--- src/shift_left/tests/it/test_metrics.py
+++ src/shift_left/tests/it/test_metrics.py
@@ -129,8 +129,11 @@ class TestConfluentMetrics(unittest.TestCase):
 
     def test_get_retention_size(self):
         print("test_get_retention_size")
-        table_name = "src_aqem_recordconfiguration_form_element"
+        table_name = "p1_fct_order"
         retention_size = metric_mgr.get_retention_size(table_name)
+        config = get_config()
+        print(f"[DEBUG] Kafka bootstrap.servers: {config['kafka']['bootstrap.servers']}")
+        print(f"[DEBUG] Topic name: {table_name}")
         print(f"retention_size: {retention_size}")
         assert retention_size > 0
     
