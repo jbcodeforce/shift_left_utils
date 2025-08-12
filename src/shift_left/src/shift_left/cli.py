678b3a6a3af3665d8c11dcc5893e5d3157746c0a
jbcodeforce <jerome.boyer@gmail.com>
2025-08-12 11:16:50 -0700

    create 0.1.31 wheel

--- src/shift_left/src/shift_left/cli.py
+++ src/shift_left/src/shift_left/cli.py
@@ -17,7 +17,7 @@ app.add_typer(table.app, name="table")
 app.add_typer(pipeline.app, name="pipeline")
 
 #__version__ = toml.load(open("pyproject.toml"))["project"]["version"]
-__version__ = "0.1.30"
+__version__ = "0.1.31"
 
 @app.command()
 def version():
